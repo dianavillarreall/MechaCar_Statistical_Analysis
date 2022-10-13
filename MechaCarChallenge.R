5b10efed437b0d1991055d6517aae199a64eecf2
dianavillarreall <103703260+dianavillarreall@users.noreply.github.com>
2022-10-13 01:49:48 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,34 +1,34 @@
-# Import librarys
+# Import library
 library(tidyverse)
 library(dplyr)
 
-# Deliverable 1: Linear Regression to Predict MPG
+# Deliverable 1
 # Import MechaCar_mpg.csv
 MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-# Perform Multiple Linear Regression
+# Multiple Linear Regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =  MechaCar_mpg)
-# Generate Summary Statistics for Linear Regression Model
+# Summary Statistics for Linear Regression Model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =  MechaCar_mpg))
 
-# Deliverable 2: Create Visualizations for the Trip Analysis 
+# Deliverable 2: Visualizations
 # Import Suspension_Coil.csv
 Coil_Data <- read.csv(file = 'Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-# Generate Summary Statistics for Suspension Coil PSI Data
+# Summary Statistics for Suspension Coil PSI Data
 Total_Summary <- Coil_Data%>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 Lot_Summary <- Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') #create summar for lot
 
 # Deliverable 3: T-Tests on Suspension Coils
-t.test(Coil_Data$PSI, mu = 1500 ) # t-test that compares all manufacturing lots against mean PSI of the population
+t.test(Coil_Data$PSI, mu = 1500 ) 
 
-#Table for Lot 1
+#Lot 1 Table
 Lot_1 <- Coil_Data %>% filter(Manufacturing_Lot == "Lot1") 
-#T Test for Lot 1
+#Lot 1 T Test
 t.test(Lot_1$PSI, mu = 1500) 
-#Table for Lot 2
+#Lot 2 Table
 Lot_2 <- Coil_Data %>% filter(Manufacturing_Lot == "Lot2") 
-#T Test for Lot 2
+#Lot 2 T Test
 t.test(Lot_2$PSI, mu = 1500) 
-#Table for Lot 3
+#Lot 3 Table
 Lot_3 <- Coil_Data %>% filter(Manufacturing_Lot == "Lot3") 
-#T Test for Lot 3
+#Lot 3 T Test
 t.test(Lot_3$PSI, mu = 1500) 
